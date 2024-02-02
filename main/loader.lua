27ee83848e830870ffa2d6345fe46cfefaa8cc51
atsuage333 <127850785+atsuage333@users.noreply.github.com>
2024-02-02 19:03:44 +0900

    Update loader.luadasdas
    dasdas

--- main/loader.lua
+++ main/loader.lua
@@ -21,7 +21,7 @@ function queueOnTeleport(str)
 end
 
 if game.CreatorId == 123247 or game.PlaceId == 370731277 and continuee == true then
-	local githublink = "https://raw.githubusercontent.com/synolope/mpcity/main/"
+	local githublink = "https://raw.githubusercontent.com/atsuage333/meepcityguijp/main/"
 	local loaderurl = githublink .. "loader.lua"
 	local uicheckurl = githublink .. "uicheck.lua"
 	local scripturl = ""
